9c8cc435fd935f667fff77fa39029e39db1cc716
Gildo Alves <gildoalvesdelimajunior@gmail.com>
2026-06-06 07:21:25 -0300

    feat: add constructor, getters and setters to Cliente, Jogo, JogoDigital e JogoFisico (#2)
    * feat: add constructor, getters and setters to clientes
    
    * feat: add constructor, getters and setters to jogo,jogoDigital and jogoFisico

--- GameRent/src/GameRent/Jogo.java
+++ GameRent/src/GameRent/Jogo.java
@@ -7,5 +7,37 @@ public abstract class Jogo {
 	private int valordiario;
 	private int classificacao;
 	private int contador;
-	
+
+	public Jogo(String nome, String plataforma, String genero, int valordiario, int classificacao, int contador){
+		this.nome = nome;
+		this.plataforma = plataforma;
+		this.genero = genero;
+		this.valordiario = valordiario;
+		this.classificacao = classificacao;
+		this.contador = contador;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public int getValordiario() {
+		return valordiario;
+	}
+
+	public int getClassificacao() {
+		return classificacao;
+	}
+
+	public int getContador() {
+		return contador;
+	}
 }

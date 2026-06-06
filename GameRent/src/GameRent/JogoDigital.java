ee2922f99954c20b6e07f2de6a33ebfe95049b11
GildoAlvesdeLimaJunior <gildoalvesdelimajunior@gmail.com>
2026-06-06 07:13:48 -0300

    feat: add constructor, getters and setters to jogo,jogoDigital and jogoFisico

--- GameRent/src/GameRent/JogoDigital.java
+++ GameRent/src/GameRent/JogoDigital.java
@@ -3,4 +3,11 @@ package GameRent;
 public class JogoDigital extends Jogo {
 	private String chaveAcesso;
 	private int tamanhoGB;
+
+	public JogoDigital(String nome, String plataforma, String genero, String chaveAcesso, int valorDiario, int classificacao, int contador, int tamanhoGB){
+		super(nome,plataforma,genero,valorDiario,classificacao,contador);
+
+		this.chaveAcesso = chaveAcesso;
+		this.tamanhoGB = tamanhoGB;
+	}
 }
--- GameRent/src/GameRent/JogoFisico.java
+++ GameRent/src/GameRent/JogoFisico.java
@@ -3,5 +3,11 @@ package GameRent;
 public class JogoFisico extends Jogo{
 	private int qntdestoque;
 	private int avaria; //a avaria varia de 1 a 5, 1 em perfeito estado e 5 muito danificado, fazendo a multa variar conforme o estado da mídia
-	
+
+	public JogoFisico(String nome, String plataforma, String genero, int qntdestoque, int valorDiario, int classificacao, int contador, int avaria){
+		super(nome,plataforma,genero,valorDiario,classificacao,contador);
+
+		this.qntdestoque = qntdestoque;
+		this.avaria = avaria;
+	}
 }

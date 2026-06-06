f4344d3c36be788bbbc591590039544465a9394f
GildoAlvesdeLimaJunior <gildoalvesdelimajunior@gmail.com>
2026-06-06 07:09:39 -0300

    feat: add constructor, getters and setters to clientes

--- GameRent/src/GameRent/Cliente.java
+++ GameRent/src/GameRent/Cliente.java
@@ -7,5 +7,42 @@ public class Cliente {
 	private int idade;
 	private boolean premium;
 	
-	
+	public Cliente(String nome, String cpf, String email, int idade, boolean premium){
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.idade = idade;
+		this.premium = premium;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public boolean isPremium() {
+		return premium;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente{" +
+				"nome='" + nome + '\'' +
+				", cpf='" + cpf + '\'' +
+				", email='" + email + '\'' +
+				", idade=" + idade +
+				", premium=" + premium +
+				'}';
+	}
 }

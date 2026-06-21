6197fc9c7642faddf0a8156849ba4270daeda2fa
Gildo Alves <gildoalvesdelimajunior@gmail.com>
2026-06-21 20:09:27 -0300

    Split Cliente into ClienteComum and ClientePremium hierarchy (#4)
    * feat: add ClienteComum and ClientePremium, now Cliente is an abstract class
    
    * feat: add main.java entry point and IntelliJ IDEA project config

---
+++ GameRent/src/main.java
@@ -0,0 +1,8 @@
+import GameRent.*;
+
+public class main {
+    public static void main(String args[]){
+        ClienteComum c1 =  new ClienteComum("João","123-456-789-10","Joaozinho123@gmail.com", 12);
+        System.out.println(c1.toString());
+    }
+}

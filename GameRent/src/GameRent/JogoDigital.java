8a1986b97f130c6cd61ced5a259dc22e51dd47f4
GildoAlvesdeLimaJunior <gildoalvesdelimajunior@gmail.com>
2026-07-10 17:46:28 -0300

    feat: add enums and availability methods to Jogo hierarchy

--- GameRent/src/GameRent/JogoDigital.java
+++ GameRent/src/GameRent/JogoDigital.java
@@ -3,11 +3,47 @@ package GameRent;
 public class JogoDigital extends Jogo {
 	private String chaveAcesso;
 	private int tamanhoGB;
+	private int limiteAcessosSimultaneos = 5; // Exemplo
+	private int acessosAtivos = 0;
 
-	public JogoDigital(String nome, String plataforma, String genero, String chaveAcesso, int valorDiario, int classificacao, int contador, int tamanhoGB){
-		super(nome,plataforma,genero,valorDiario,classificacao,contador);
+	public JogoDigital(String nome, Plataforma plataforma, String genero, String chaveAcesso, int valorDiario, ClassificacaoEtaria classificacao, int tamanhoGB){
+		super(nome,plataforma,genero,valorDiario,classificacao);
 
 		this.chaveAcesso = chaveAcesso;
 		this.tamanhoGB = tamanhoGB;
+	}
+
+	public boolean isDisponivel(){
+		if(limiteAcessosSimultaneos > acessosAtivos){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public boolean reservarAcesso(){
+		if(limiteAcessosSimultaneos > acessosAtivos){
+			acessosAtivos++;
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public boolean liberarAcesso(){
+		if(acessosAtivos > 0){
+			acessosAtivos--;
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public String getChaveAcesso() {
+		return chaveAcesso;
+	}
+
+	public int getTamanhoGB() {
+		return tamanhoGB;
 	}
 }

6b78d340269827ec85b39a33bd92fb4554e45473
williamGomesNice <william.severo@aluno.ufca.edu.br>
2026-07-18 05:07:31 -0300

    Informação dos pontos de fidelidadr adicionado ao método toString() do cliente

--- GameRent/src/GameRent/Cliente.java
+++ GameRent/src/GameRent/Cliente.java
@@ -39,7 +39,8 @@ public abstract class Cliente {
 				"nome='" + nome + '\'' +
 				", cpf='" + cpf + '\'' +
 				", email='" + email + '\'' +
-				", idade=" + idade +
+				", idade=" + idade + '\'' +
+				", pontos de fidelidade=" + fidelidade.getPontos() + 
 				'}';
 	}
 
